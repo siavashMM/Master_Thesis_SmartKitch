b5134cb3efb0d1be94337823119f2d42c1659c02
Siavash Maghsody Moghadam <siavashmaghsodymoghadam@40bb0feb-a417-4eeb-bbec-d6216e380468.fritz.box>
2025-07-13 21:43:39 +0200

    Cognito

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Amplify } from 'aws-amplify'
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_pveKN6Tw5',
+      userPoolClientId: '540va5rijbq8jn37234iverd4f'
+    }
+  }
+});
 
 @NgModule({
   declarations: [
@@ -10,7 +21,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
